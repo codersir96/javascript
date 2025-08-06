d05ab4476bd2ab70ccb503cb4b3377b5d382db93
codersir96 <satyamgiri9569@gmail.com>
2025-08-06 16:43:45 +0000

    02_datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict";//treat all js as newest version
+
+//alert("hello"). we are using js not browser
+
+
+console.log(8
+    +9
+)//code readiability should be high
+
+let age=23
+let name="Satyam"
+let isLoggedIn=false;
+
+
+//number => 2 to power 52
+//bigint
+// string =>
+// boolean => true or false
+//null=>standalone value
+//undefined    
+
+
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
