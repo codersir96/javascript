eaa5eb13c634ddeda7acfee45c7da729035abf77
codersir96 <satyamgiri9569@gmail.com>
2025-08-06 16:26:43 +0000

    01 var

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=2324
+let accountEmail="Satyam@gmail.com"
+var accountPassword="1234"
+  
+accountCity="Delhi"
+/*
+not prrefer to use var because of isssue in block and functional scope
+
+*/
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
